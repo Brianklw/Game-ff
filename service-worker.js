9aecab62547d69137dec7b1c6273fb5bba3c9583
Brianklw <brian.ko@evoke-ai.co>
2025-08-08 05:44:44 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('cc-v1').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.webmanifest',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
